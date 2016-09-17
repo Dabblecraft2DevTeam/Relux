f1692140a1f5c6ab7cb5a3fdad1ab7d02c3ffd95
WhiteWolfdoge <emjwhite@outlook.com>
2016-09-17 13:23:41 -0700

    Added specific prefix for exceptions

--- src/net/whitewolfdoge/relux/ReluxPlugin.java
+++ src/net/whitewolfdoge/relux/ReluxPlugin.java
@@ -10,16 +10,18 @@ public class ReluxPlugin extends JavaPlugin{
 	protected static final int
 		MIN_RADIUS =	1,
 		MAX_RADIUS = 	15;
-	protected static final String 
+	protected static final String
 		CMD_MAIN =						"relux",
 		PERMISSION_USE =				"relux.use",
 		MSG_PREFIX =					ChatColor.LIGHT_PURPLE + "[Relux] " + ChatColor.YELLOW,
 		MSG_INFO =						MSG_PREFIX + "Relux allows manual chunk relighting. Written by WhiteWolfdoge (Emily White)",
 		MSG_USAGE =						MSG_PREFIX + "Usage: " + ChatColor.ITALIC + "/relux <radius> [<xPos> <zPos>]",
-		MSG_EX_PERMISSION_DENIED =		MSG_PREFIX + ChatColor.RED + "You were denied permission to use this plugin.",
-		MSG_EX_ARGS_INVALID_QTY =		MSG_PREFIX + ChatColor.RED + "Invalid quantity of arguments!",
-		MSG_EX_ARGS_INVALID_RADIUS =	MSG_PREFIX + ChatColor.RED + "The radius must be an integer in the range of " + MIN_RADIUS + " through " + MAX_RADIUS,
-		MSG_EX_INVALID_SOURCE =			MSG_PREFIX + ChatColor.RED + "You must use this command from within a world.";
+		
+		MSG_EX_PREFIX =					MSG_PREFIX + ChatColor.RED,
+		MSG_EX_PERMISSION_DENIED =		MSG_EX_PREFIX + "You were denied permission to use this plugin.",
+		MSG_EX_ARGS_INVALID_QTY =		MSG_EX_PREFIX + "Invalid quantity of arguments!",
+		MSG_EX_ARGS_INVALID_RADIUS =	MSG_EX_PREFIX + "The radius must be an integer in the range of " + MIN_RADIUS + " through " + MAX_RADIUS,
+		MSG_EX_INVALID_SOURCE =			MSG_EX_PREFIX + "You must use this command from within a world.";
 	
 	// TODO write doc
 	@Override
